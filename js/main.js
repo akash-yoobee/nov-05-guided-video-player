ab3aed6e6184047b94e76c0dbc8a9cf74deb8aa8
Akash Sanyal <270023449@col573-l13-06.local>
2019-11-06 18:54:28 +1300

    committing changes made on nov 6

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
   const EL_VIDEO_LIST = $('#video-list'),
-  EL_PLAYER = $('#player')
-  EL_SEARCH_BOX = $('#search-box');
+  EL_PLAYER = $('#player'),
+  EL_SEARCH_BOX = $('#search-box'),
+  EL_CATEGORY_LIST = $('#category-list');
+  // EL_CATEGORY_SEARCH_BOX = $('#search-category-box');
   let videosArr = [];
 
   // https://www.w3schools.com/jquery/ajax_getjson.asp
@@ -10,24 +12,40 @@ $(document).ready(function() {
     $.getJSON('json/videos.json', function(data){
       videosArr = data.videos;
       displayVideos(videosArr);
-      // addClickListners();
+    });
+    // Akash Parameters only exist inside the function they are being passed into
+    // The anonymous function is called a callback function
+    // callback function = function that is called when the method/or function finishes executing its task
+    $.getJSON('json/categories.json', function(data){
+      let categoriesArr = data.categories;
+      displayCategories(categoriesArr);
     });
     EL_SEARCH_BOX.on('keyup', function(event){
       event.preventDefault();
       displayVideosByTitle($(this).val());
     });
+    // EL_CATEGORY_SEARCH_BOX.on('keyup', function(event){
+    //   event.preventDefault();
+    //   displayVideosByCategory($(this).val());
+    // });
   }
-  
-  function displayVideos(videos){
-  let string ='';
-  $.each(videos, function(i, video) {
-    string += getVideoHtml(video);
-  });
-
-  EL_VIDEO_LIST.html(string);
-  addClickListners();
+    function displayVideos(videos){
+    let string ='';
+    $.each(videos, function(i, video) {
+      string += getVideoHtml(video);
+    });
+    EL_VIDEO_LIST.html(string);
+    addClickListners();
   }
-  
+  function displayCategories(categoriesArr){
+    let string ='';
+    // Anon Fx here is getting two things the .each method passes - index & category
+    $.each(categoriesArr, function(i, categoriesArr) {
+      string += getCategoryHtml(categoriesArr);
+    });
+    EL_CATEGORY_LIST.html(string);
+    // addClickListners();
+  }
   function getVideoHtml(video){
     return `<div class="video-item" data-id="${video.id}">
       <div>
@@ -40,14 +58,14 @@ $(document).ready(function() {
     </div>
     `
   }
+  function getCategoryHtml(categoriesArr){
+    return `<li><h4>${categoriesArr.title}</h4></li>
+    `
+  }
   function addClickListners(){
     $('.video-item').on('click', function(){
       let videoId = $(this).data('id');
       playVideo(videoId);
-
-      // getting the HTML data attribute called id out 
-      // let id = $(this).data('id')
-      // EL_PLAYER.attr('src', `https://www.youtube.com/embed/${id}?rel=0&modestbranding=1&autohide=1&mute=0&showinfo=0&controls=0&autoplay=1`)
     });
   }
 
@@ -57,7 +75,6 @@ $(document).ready(function() {
 
   function displayVideosByTitle(title){
     let filteredVideos = [];
-    
     $.each(videosArr, function(i, video) {
     if (video.title.includes(title)) {
       filteredVideos.push(video);
@@ -65,6 +82,14 @@ $(document).ready(function() {
     });
     displayVideos(filteredVideos);
   }
-
+  // function displayVideosByCategory(category){
+  //   let filteredVideos = [];
+  //   $.each(videosArr, function(i, video) {
+  //   if (video.category.includes(category)) {
+  //     filteredVideos.push(video);
+  //     }
+  //   });
+  //   displayVideos(filteredVideos);
+  // }
   init();
 })
